76c923d73af664ed4f391ebc5c864ec630c875de
alexandriamw <alexandriawagner84@gmail.com>
2019-12-08 22:18:43 -0600

    finished the clock

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ function setDate() {
     console.log(minutes);
     const minutesDegrees = ((minutes / 60) * 360) + 90;
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hours = now.getHours();
+    console.log(hours);
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
